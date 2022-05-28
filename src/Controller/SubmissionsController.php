11be5120277d7fa332fdf8fbc40c107d17b7cbf5
Jean Luca Bez <jeanlucabez@gmail.com>
2022-05-28 15:06:47 -0700

    Fix typo

--- src/Controller/SubmissionsController.php
+++ src/Controller/SubmissionsController.php
@@ -141,7 +141,7 @@ class SubmissionsController extends AppController
         $submission->io500_score = isset($json_io500['att']['score']) ? $json_io500['att']['score'] : null;
         $submission->original_io500_score = isset($json_io500['att']['score']) ? $json_io500['att']['score'] : null;
 
-        $submission->information_10_node_challenge = $json_io500['att']['include_in_10node_ranked_list']) == 'yes';
+        $submission->information_10_node_challenge = $json_io500['att']['include_in_10node_ranked_list'] == 'yes';
         $submission->include_in_io500 = $json_io500['att']['include_in_io500_ranked_list']) == 'yes';
 
         if ($new_format) {
